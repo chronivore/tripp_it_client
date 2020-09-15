10bdbb88d625d600c23f4b8a2ce4389798165f77
Nate Gropp <nathangropp@gmail.com>
2020-09-15 13:37:03 -0400

    change url

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Login from './auth/Login';
 import Auth from './auth/Auth';
 import Home from './home/Homepage';
+import Trips from './home/Homepage';
 
 function App() {
   const [sessionToken, setSessionToken] = useState('');
@@ -27,7 +28,7 @@ function App() {
 
   return (
     <div>
-      {!sessionToken ? <Auth updateToken={updateToken}/> : <Home sessionToken={sessionToken}/>}
+      {!sessionToken ? <Auth updateToken={updateToken}/> : <Trips />}
     </div>
   );
 }
--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -3,10 +3,10 @@ let APIURL = '';
 switch (window.location.client) {
 
     case 'localhost' || '127.0.0.1':
-        APIURL = 'http://localhost:3000';
+        APIURL = 'http://localhost:4000';
         break;
     case 'ana-tripp-it-client.herokuapp.com':
-        APIURL = 'https://ana-tripp-it-client.herokuapp.com/'
+        APIURL = 'https://ana-tripp-it-server.herokuapp.com/'
 }
 
 export default APIURL;
